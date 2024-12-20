c1d9ad47b71b277ab06a23576c7627311728c4c3
Jack9491 <jackflynn94@gmail.com>
2024-12-20 03:18:46 +0000

    Commit 4.1: Added HomeViewModel back in as I accidentally deleted it.

---
+++ app/src/main/java/ie/tus/libloans/viewmodel/HomeViewModel.kt
@@ -0,0 +1,9 @@
+package ie.tus.libloans.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class HomeViewModel(application: Application) : AndroidViewModel(application) {
+
+    //private val repository = LibLoansRepository(application)
+}
